5397c75d2cfe794da129567ead989e25e7db0bd6
bcloutier412 <brandoncloutier412@gmail.com>
2024-08-07 10:20:45 -0700

    finished 973 k closest points to origin

--- medium-solutions/973-k-closest-points-to-origin.js
+++ medium-solutions/973-k-closest-points-to-origin.js
@@ -1,4 +1,7 @@
 /**
+ * MIN HEAP IMPLEMENTATION
+ */
+/**
  * @param {number[][]} points
  * @param {number} k
  * @return {number[][]}
@@ -28,4 +31,46 @@ var kClosest = function(points, k) {
  * First I was thinking okay I can use a minHeap and I could add all the values in the array to the min heap and the priority
  * would be calculated by finding the distance from the point to the origin. I would then enqueue kth number of elements from the heap
  * and push those to an ouput array. I would then return the ouput array.
+ */
+
+/**
+ * MAX HEAP IMPLEMENTATION
+ */
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+  /**
+   * Comments: We want to create a MaxHeap and iterate through the points array and compare the distance to the top element. If it is less
+   * then, then we want to dequeue the top element and add the new element. If the MaxHeap is underCapacity then we will just add the element.
+   * Pretty much the top element is the bound between the k closest elements to the origin and the other elements.
+   */
+  const maxHeap = new MaxPriorityQueue()
+
+  points.forEach(point => {
+      const priority = Math.round(Math.sqrt((point[0] * point[0]) + (point[1] * point[1])) * 10 * 10 * 10 * 10)
+
+      if (maxHeap.size() < k) {
+          maxHeap.enqueue(point, priority)
+          return
+      }
+
+      if (priority < maxHeap.front().priority) {
+          maxHeap.dequeue()
+          maxHeap.enqueue(point, priority)
+      }
+  })
+
+  return maxHeap.toArray().map(i => i.element)
+};
+
+/**
+ * Solution Notes:
+ * In this solution I decided to use a maxheap of a fixed size k. A MaxHeap is used when you want to be able to see the larget priority element
+ * in the datastructure in O(1) time. This works for this solution because I want to keep a group of k number of elements and only need to check the largest element in the group.
+ * We will then iterate through all the points and compare it to the largest. If it is smaller than the largest then we will replace the new point with the top element.
+ * The time complexity of dequeuing from a MaxHeap is O(logn) and enqueuing is O(logn). What is nice about this solution is we get to skip some of the values in
+ * the inputed array. Worst case we would get a O(nlogn + klogn). In the best case we will get a O(2klogn) 
  */
